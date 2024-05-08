ae6953deb9bd37cff6fe7a68d99c633bac9b6afe
Willow Maccagnoni <willow.maccagnoni@gmail.com>
2024-05-08 19:28:05 +0200

    Conditional styles were missing too

--- styles.go
+++ styles.go
@@ -1,4 +1,20 @@
 package gomplements
 
-// Styles allows defining CSS styles to apply to an element.
+// Styles allows to define CSS styles to apply to an Element.
 type Styles map[string]string
+
+// If allows to apply CSS styles to an Element, only if a condition is met.
+func (s Styles) If(cond bool) ParentModifier {
+	return &conditionalStyles{styles: s, cond: cond}
+}
+
+type conditionalStyles struct {
+	styles Styles
+	cond   bool
+}
+
+func (c *conditionalStyles) ModifyParent(p Element) {
+	if c.cond {
+		p.With(c.styles)
+	}
+}
