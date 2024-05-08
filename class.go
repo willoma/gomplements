c282e9515f5226fc397e2f5ed49534152917c166
Willow Maccagnoni <willow.maccagnoni@gmail.com>
2024-05-08 18:33:42 +0200

    add the If method to the Class object, thus providing conditional classes
    Implements #1

--- class.go
+++ class.go
@@ -1,7 +1,23 @@
 package gomplements
 
-// Class represents a CSS class, which is applied in the `class` attribute of an HTML element.
+// Class represents a CSS class, which is applied in the class attribute of a Element.
 type Class string
+
+// If allows to add a CSS class to an Element, only if a condition is met.
+func (c Class) If(condition bool) ParentModifier {
+	return &conditionalClass{class: c, cond: condition}
+}
+
+type conditionalClass struct {
+	class Class
+	cond  bool
+}
+
+func (c conditionalClass) ModifyParent(p Element) {
+	if c.cond {
+		p.With(c.class)
+	}
+}
 
 // Classes contains a list of CSS classes to be applied in the `class` attribute of an HTML element.
 type Classes []Class
