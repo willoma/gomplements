33704bc8fe874705d04c502676d543ca7fd0a8a5
Willow Maccagnoni <willow@elendil.fr>
2024-10-09 13:06:53 +0200

    Add the RawScript helper

--- helpers.go
+++ helpers.go
@@ -1,6 +1,7 @@
 package gomplements
 
 import (
+	"github.com/maragudk/gomponents"
 	"github.com/maragudk/gomponents/html"
 )
 
@@ -18,4 +19,9 @@ type ID string
 // ImgSrc creates a <img> element, with the provided src.
 func ImgSrc(src string, children ...any) Element {
 	return Elem(html.Img, html.Src(src), children)
+}
+
+// RawScript creates a <script> element, with the provided content as a script, unmodified.
+func RawScript(script string) Element {
+	return Script(gomponents.Raw(script))
 }
